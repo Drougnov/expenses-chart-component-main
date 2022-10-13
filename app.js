2aff7ecee3ca821d05b0cc8d7274c3c7e4563299
RockyBaura <251drougnov@gmail.com>
2022-10-13 21:30:59 +0600

    Added hover effect with JS

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const chartBars = document.querySelectorAll('.chart__bar');
-
-let date = new Date();
-let currentDay = date.getDay();
-
+const date = new Date();
+const currentDay = date.getDay();
 
 fetch('data.json')
     .then(res => res.json())
     .then(data => {
         for(let i=0; i<7; i++){
-            chartBars[i].style.height = `${data[i].amount * 2.5}px`;
-            chartBars[i].style.setProperty('--content',`"$${data[i].amount}"`);
-            chartBars[currentDay - 1].style.background = "hsl(186, 34%, 60%)";
+            chartBars[i].style.height = `${data[i].amount * 2.5}px`;                //give 2.5 x amount height to bars 
+            chartBars[i].style.setProperty('--content',`"$${data[i].amount}"`);     //replace after element's content with amount
+            chartBars[currentDay - 1].style.background = "hsl(186, 34%, 60%)";      //cyan background to the current day
+
+            //hover effects for the current day's bar
+            chartBars[currentDay - 1].addEventListener('mouseenter',()=>{
+                chartBars[currentDay - 1].style.background = "hsl(186, 34%, 70%)";
+            })
+            chartBars[currentDay - 1].addEventListener('mouseleave',()=>{
+                chartBars[currentDay - 1].style.background = "hsl(186, 34%, 60%)";
+            })
+            
+            //hover effects for the bars
+            chartBars[i].addEventListener('mouseover',(e)=>{
+                chartBars[i].style.background = "hsl(10, 79%, 75%)";
+            })
+            chartBars[i].addEventListener('mouseleave',(e)=>{
+                chartBars[i].style.background = "hsl(10, 79%, 65%)";
+            })
+
         }
     })
