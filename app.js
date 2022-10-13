0e92ea08d9025d242f7de443f748a823f2eb3a7d
RockyBaura <251drougnov@gmail.com>
2022-10-13 23:19:38 +0600

    Code repositioned

--- app.js
+++ app.js
@@ -8,22 +8,22 @@ fetch('data.json')
         for(let i=0; i<7; i++){
             chartBars[i].style.height = `${data[i].amount * 2.5}px`;
             chartBars[i].style.setProperty('--content',`"$${data[i].amount}"`);     //replace after element's content with amount
-            chartBars[currentDay - 1].style.background = "hsl(186, 34%, 60%)";
+            
+            chartBars[i].addEventListener('mouseover',(e)=>{
+                chartBars[i].style.background = "hsl(10, 79%, 75%)";
+            })
+            chartBars[i].addEventListener('mouseleave',(e)=>{
+                chartBars[i].style.background = "hsl(10, 79%, 65%)";
+            })
 
+            //current day bar
+            chartBars[currentDay - 1].style.background = "hsl(186, 34%, 60%)";      //minus one cause data started with monday instead of sunday
 
             chartBars[currentDay - 1].addEventListener('mouseenter',()=>{
                 chartBars[currentDay - 1].style.background = "hsl(186, 34%, 70%)";
             })
             chartBars[currentDay - 1].addEventListener('mouseleave',()=>{
                 chartBars[currentDay - 1].style.background = "hsl(186, 34%, 60%)";
-            })
-
-
-            chartBars[i].addEventListener('mouseover',(e)=>{
-                chartBars[i].style.background = "hsl(10, 79%, 75%)";
-            })
-            chartBars[i].addEventListener('mouseleave',(e)=>{
-                chartBars[i].style.background = "hsl(10, 79%, 65%)";
             })
         }
     })
