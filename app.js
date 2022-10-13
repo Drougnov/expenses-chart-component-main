a9f9016b75578f6ac72d6166e486482f8ebf06c1
RockyBaura <251drougnov@gmail.com>
2022-10-13 17:12:54 +0600

    Added style to current day's bar

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const chartBars = document.querySelectorAll('.chart__bar');
 
+let date = new Date();
+let currentDay = date.getDay();
+
+
 fetch('data.json')
     .then(res => res.json())
     .then(data => {
         for(let i=0; i<7; i++){
-            chartBars[i].style.height = `${data[i].amount * 2}px`;
+            chartBars[i].style.height = `${data[i].amount * 2.5}px`;
             chartBars[i].style.setProperty('--content',`"$${data[i].amount}"`);
+            chartBars[currentDay - 1].style.background = "hsl(186, 34%, 60%)";
         }
     })
